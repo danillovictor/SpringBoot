178e334ea8d38b724f3ff83efd83acb432b5dfe6
Danilo Oliveira <danilotfm18@gmail.com>
2020-07-29 15:05:06 -0300

    Desenvolvimento do Spring

---
+++ escola/src/main/java/com/minha/escola/model/Turma.java
@@ -0,0 +1,48 @@
+package com.minha.escola.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "tb_turma")
+public class Turma {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String descricao;
+	
+	private boolean ativo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	
+}
---
+++ escola/src/main/java/com/minha/escola/repository/TurmaRepository.java
@@ -0,0 +1,16 @@
+package com.minha.escola.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.minha.escola.model.Turma;
+
+import java.util.List;
+
+
+@Repository
+public interface TurmaRepository extends JpaRepository<Turma, Long>  {
+	
+	public List<Turma> findAllByDescricaoContainingIgnoreCase (String descricao); 
+
+}
